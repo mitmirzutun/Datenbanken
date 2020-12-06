6a89fb04205c0512075eb6b41a32c9ef06b783f8
mitmirzutun <milan1111@gmx.de>
2020-12-06 14:13:36 +0100

    update

--- sevenDatabsesInSevenWeeks/Datenbanken/chapter2.sql
+++ sevenDatabsesInSevenWeeks/Datenbanken/chapter2.sql
@@ -15,3 +15,5 @@ create table events(title text, starts timestamp, ends timestamp, venue_id integ
 insert into events (title, starts, ends, venue_id) values ('LARP Club', '2012-02-15 17:30:00', '2012-02-15 19:30:00', 2);
 insert into events (title, starts, ends) values ('April Fools Day', '2012-04-01 00:00:00', '2012-04-01 23:59:00');
 insert into events (title, starts, ends) values ('Christmas', '2012-12-25 00:00:00', '2012-12-25 23:59:00');
+select relname from pg_class where relnamespace='2200' and relkind='r';
+select c.country_name from countries c right join (select v.country_code from venues v left join events e on v.venue_id=e.venue_id where e.title='LARP Club') as v on v.country_code=c.country_code;
