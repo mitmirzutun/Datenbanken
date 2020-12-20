88d9abe9bd8232c1ec36882df0486f126dba528e
ynux <ynux@gmx.net>
2020-12-20 18:27:36 +0100

    typo in extension

--- sevenDatabsesInSevenWeeks/Datenbanken/chapter2.sql
+++ sevenDatabsesInSevenWeeks/Datenbanken/chapter2.sql
@@ -9,7 +9,7 @@ create database book;
 
 create extension tablefunc;
 create extension dict_xsyn;
-create extension fuzzystmatch;
+create extension fuzzystrmatch;
 create extension pg_trgm;
 create extension cube;
 
