d94e876a0518d559c54380c322d1fb543abedb55
ynux <ynux@gmx.net>
2021-01-05 19:53:41 +0100

    do 3 done (i think)

--- sevenDatabsesInSevenWeeks/Datenbanken/homework_day2_do.sql
+++ sevenDatabsesInSevenWeeks/Datenbanken/homework_day2_do.sql
@@ -10,3 +10,6 @@ select * from venues;
 select * from generate_series(1,12);
 Select * from crosstab('select extract(year from starts) as year, extract(month from starts) as month, count(*) from events group by year, month order by year, month', 'select * from generate_series(1,12)') as (year int, jan int, feb int, mar int, apr int, may int, jun int, jul int, aug int, sep int, oct int, nov int, dec int) order by year; 
 
+/* Build a pivot table that displays every day in a single month, where each week of the month is a row and each day name forms a column,. Each day should contain a count of the nuber of events for that date or sould remain blank oíf no event occurs. */ 
+
+select * from crosstab('select extract(month from starts) as month, extract(dow from starts) as weekday, count(*) from events group by month, weekday','select generate_series(1,7)') as (month int, sun int, mon int, tue int, wed int, thu int, fri int, sat int);
